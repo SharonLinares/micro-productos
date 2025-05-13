340c6f3bf93166124b891646f0f87f5c13a8bcf0
SharonLinares <156675547+SharonLinares@users.noreply.github.com>
2025-05-13 12:54:54 +0200

    Add files via upload

---
+++ src/main/java/com/example/demo/service/ProductoService.java
@@ -0,0 +1,22 @@
+package com.example.demo.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.demo.dto.ProductoDto;
+
+public class ProductoService {
+
+	private List<ProductoDto> productosDto = new ArrayList<>();
+
+
+
+	public void agregarProducto(ProductoDto productoDto) {
+		this.productosDto.add(productoDto);
+	}
+
+	public List<ProductoDto> consultarProductos() {
+		return this.productosDto;
+	}
+
+}
